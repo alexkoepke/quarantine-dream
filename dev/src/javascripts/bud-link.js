847806d5e06eaf5469ff7cab6f77d057317f2809
alexkoepke <alex.koepke@kasisto.com>
2020-05-22 17:09:59 -0400

    update site

--- dev/src/javascripts/bud-link.js
+++ dev/src/javascripts/bud-link.js
@@ -1,9 +1,24 @@
 const $ = require('jquery');
 
-const budNumb = Math.round(Math.random() * 10);
+const budLink = () => {
+  const budNumb = Math.round(Math.random() * 30);
 
-if (budNumb % 2 === 0) {
-  $('.bud-link').attr('href', 'https://youtu.be/JJmqCKtJnxM');
-} else {
-  $('.bud-link').attr('href', 'https://youtu.be/qwA7_aawH5E');
-}
+  if (budNumb >= 0 && budNumb <= 9) {
+    $('.bud-link').attr('href', 'https://youtu.be/JJmqCKtJnxM');
+  } else if (budNumb >= 10 && budNumb <= 19) {
+    $('.bud-link').attr('href', 'https://youtu.be/qwA7_aawH5E');
+  } else {
+    $('.bud-link').attr('href', 'https://d3qvyul2tp4j8.cloudfront.net/i/WwlzCMhZDu.jpg');
+  }
+};
+
+budLink();
+
+let index = 0;
+setInterval(() => {
+  if (index === 3) {
+    index = 0;
+  }
+  budLink();
+  index += 1;
+}, 2000);
